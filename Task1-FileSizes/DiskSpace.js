23319d06b5ba9a74b95ae122ac38b2b2668ff4a5
valentinasoima24 <126244414+valentinasoima24@users.noreply.github.com>
2023-07-13 14:41:15 +0300

    Delete DiskSpace.js

--- Task1-FileSizes/DiskSpace.js
+++
@@ -1,8 +0,0 @@
-function computeSizeOnDisk(clusterSize, fileSize) {
-    const clusterCount = Math.ceil(fileSize / clusterSize);
-    return clustersNumber * clusterSize;
-}
-
-const clusterSize = 512;
-const fileSize = 1500;
-computeSizeOnDisk(clusterSize, fileSize);
